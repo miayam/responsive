c84ca4b07ca0bd946f8bca4b5e520d0f7498c45f
Muhammad D. R <muhammaddeni90@gmail.com>
2022-04-03 19:19:01 +0700

    feat: Import color palettes from CSS Modules.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import Column from './components/atoms/Column';
+import Row from './components/atoms/Row';
+import * as palettesModule from './index.module.scss';
+
+const { default: palettes } = palettesModule;
+
+/* eslint-disable */
+console.log(palettes);
+
 function App() {
   return (
     <div className="App">
-      Hello
+      <Column>
+        <Row>
+          <Column>
+            <h1>Title</h1>
+            <p>Hello</p>
+          </Column>
+          <Column>
+            <h1>Title 2</h1>
+            <p>Hello 2</p>
+          </Column>
+        </Row>
+        <Row>
+          Hello Bro!
+        </Row>
+      </Column>
     </div>
   );
 }
