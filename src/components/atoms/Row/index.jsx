0a1ec5cf001996031769f2f0d07fd41731255900
Muhammad D. R <muhammaddeni90@gmail.com>
2022-04-03 17:32:31 +0700

    feat: Folder structure.

---
+++ src/components/atoms/Row/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Row = ({ children }) => {
+  return (
+    <div className="a-row">
+      {children}
+    </div>
+  );
+};
+
+export default Row;
---
+++ src/components/molecules/StackedBlocks/index.jsx
@@ -0,0 +1,1 @@
+// Can generate stacked blocks
