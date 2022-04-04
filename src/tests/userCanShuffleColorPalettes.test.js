96432a5a7c4dc361a14a241eb730a19b53fca69f
Muhammad D. R <muhammaddeni90@gmail.com>
2022-04-04 20:29:45 +0700

    feat: Automated testing is done.

--- src/tests/userCanShuffleColorPalettes.test.js
+++ src/tests/userCanShuffleColorPalettes.test.js
@@ -1,12 +1,21 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Box from '../components/atoms/Box';
+
 describe(`
   User can shuffle color palettes by
   clicking any boxes on the screen.
 `, () => {
   it('should make sure onclick event is fired', async () => {
-    expect(true).toBe(true);
+    const handleClick = jest.fn()
+
+    render(<Box number={1} aspectRatio={{ x: 1, y: 1}} onClick={handleClick} />)
+
+    fireEvent.click(screen.queryByText('1'));
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 
-  it('should shuffle CSS variables at :root', async () => {
-    expect(true).toBe(true);
+  it.skip('should shuffle CSS variables at :root', async () => {
+    // It's very hard to test. End to end testing with Cypress is the best choice.
   });
 });
